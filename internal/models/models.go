df8489ed2f3c80ee5393a80d2a23acd322ae7d56
yoshinoseiji <sij.ysn25@gmail.com>
2022-01-30 18:41:31 +0900

    Connecting DB

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,61 @@
 package models
 
-// Reservation holds reservation data
+import "time"
+
+// User is the users model
+type User struct {
+	ID int
+	FirstName string
+	LastName string
+	Email string
+	Password string
+	AccessLevel int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Room is the rooms model
+type Room struct {
+	ID int
+	RoomName string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restriction is the restrictions model
+type Restriction struct {
+	ID int
+	RestrictionName string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Reservation is the reservations model
 type Reservation struct {
+	ID int
 	FirstName string
 	LastName string
 	Email string
 	Phone string
+	StartDate time.Time
+	EndDate time.Time
+	RoomID int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room Room
+}
+
+// RoomRestriction is the room restrictions model
+type RoomRestriction struct {
+	ID int
+	StartDate time.Time
+	EndDate time.Time
+	RoomID int
+	ReservationID int
+	RestrictionID int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room Room
+	Reservation Reservation
+	Restriction Restriction
 }
---
+++ internal/repository/repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "github.com/sijysn/bookings/internal/models"
+
+type DatabaseRepo interface {
+	AllUsers() bool
+
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+}
